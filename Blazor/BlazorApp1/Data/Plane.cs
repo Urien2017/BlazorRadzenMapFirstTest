1c1bcf658b65d72a25bc98d313d8b87a1c47f65c
Urien2017 <32823418+Urien2017@users.noreply.github.com>
2022-08-22 10:56:06 +0100

    Initial commit

---
+++ Blazor/BlazorApp1/Data/Plane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class Plane
+    {
+        public int Id { get; set; }
+        public string PlaneName { get; set; }
+        public decimal TicketPrice { get; set; }
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Blazor/BlazorApp1/Data/PlaneServices.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class PlaneServices
+    {
+       private readonly AppDbcontext _context;
+        public PlaneServices(AppDbcontext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Plane>> GetPlaneAsync()
+        {
+            return await _context.Planes.ToListAsync();
+        }
+
+        public async Task<Plane> AddPlaneAsync(Plane plane)
+        {
+            try
+            {
+                _context.Planes.Add(plane);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return plane;
+        }
+
+        public async Task<Plane> UpdatePlaneAsync(Plane plane)
+        {
+            try
+            {
+                var productExist = _context.Planes.FirstOrDefault(p => p.Id == plane.Id);
+                if (productExist != null)
+                {
+                    _context.Update(plane);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return plane;
+        }
+
+        public async Task DeletePlaneAsync(Plane plane)
+        {
+            try
+            {
+                _context.Planes.Remove(plane);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
